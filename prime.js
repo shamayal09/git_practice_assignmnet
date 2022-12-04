d70fa5a2fe0b9df0738f58238192176035dbd993
shamayal09 <shamayalnabi1995@gmail.com>
2022-12-04 21:58:39 +0530

    prime

--- prime.js
+++ prime.js
@@ -1,4 +1,31 @@
 
-let b = 2
-let a = 1;
-console.log(a+b)
+function check_prime(num){ let count=0;
+
+    for(let i=1; i<=num; i++){
+    
+    if(num%i==0){
+    
+    count++;
+    
+    }
+    
+    }
+    
+    if(count==2){
+    
+    console.log("prime number")
+    
+    }else if
+    {
+    
+    console.log('not a prime number')
+    
+      
+    
+    }
+    } 
+      
+    check_prime(10);
+    
+    check_prime(19);
+    
