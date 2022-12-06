b1e86e7752cb87bd8b28e16987022ae3315bda28
shamayal09 <shamayalnabi1995@gmail.com>
2022-12-06 18:44:53 +0530

    pallindrom

---
+++ pallindrom.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+    let i = 0;
+    
+    let j = str.length - 1;
+    
+       while ( i < j){
+         if (str[i] != str[j]){ 
+           return console.log("No")
+           
+      }
+    
+    i++;
+    j--
+    }
+    
+      console.log("Yes")
+      
+    
+    
